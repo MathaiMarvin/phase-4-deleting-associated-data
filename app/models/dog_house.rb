88093d586083c3cb4df7930b7d212754263bc8ea
m-a-t-h-a-i <mathaimarvin574@gmail.com>
2023-03-21 14:23:49 +0300

    Completed assignment

--- app/models/dog_house.rb
+++ app/models/dog_house.rb
@@ -1,3 +1,7 @@
 class DogHouse < ApplicationRecord
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
+
+  # ha_many association reference gives you additional options to customize its behavior, we can use the dependent: :destroy
+  # The above tells Active Record to delete all the associated recors when parent record is deleted
+
 end
